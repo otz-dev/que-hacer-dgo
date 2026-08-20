0a8c7703c2f098a2d28e335a94f64e91582f80a3
otz-dev <soniaortiz.dev@gmail.com>
2026-08-20 14:16:39 -0600

    QAED: Added event detail and poster

--- src/ListaDeEventos/Lista.jsx
+++ src/ListaDeEventos/Lista.jsx
@@ -1,14 +1,45 @@
+import { useState } from 'react';
 import './index.css';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export const Lista = (props)=>{
+    const [selectedEventDetails, setSelectedEventDetails] = useState('');
+
     return <div className='lista-eventos'>
             <ul>
             {props.eventos?.map((evento)=>{
-                return (<li key={evento.id}>
-                    <div className="event-type" id={`event-type-item-${evento.id}`}/>
-                    <span><strong>{new Date(evento.start).toLocaleTimeString()}</strong></span>
-                    <span className='event-title-list-item'>{evento.title}</span>
-                    </li>)
+                return (
+                    <>
+                    <li key={evento.id} onClick={(e)=>{
+                        setSelectedEventDetails((prev)=>prev!==evento.id ? evento.id : '')
+                    }}>
+                    
+                        <div className={`view-event-detail-collapsed ${ selectedEventDetails === evento.id? 'view-event-detail-expanded' : ''} `}>
+                        <ArrowForwardIosIcon />
+
+                        </div>
+                        <div className="event-type" id={`event-type-item-${evento.id}`}/>
+                        <span className='event-time'><strong>{new Date(evento.start).toLocaleTimeString()}</strong></span>
+                        <span className='event-title-list-item'>{evento.title}</span>
+                        
+                    
+                    </li>
+                        <div className='event-detail-card' hidden={selectedEventDetails !== evento.id}>
+                            <h3>{evento.title}</h3>
+
+                            <div className='event-info-container'>
+                                <div><span className='description'><strong>Lugar: </strong></span>{evento.placeName}</div>
+                                <div><span className='description'><strong>Dirección: </strong></span>{evento.address}</div>
+                                <div><span className='description'><strong>Teléfono: </strong></span>{evento.price}</div>
+                                <div><span className='description'><strong>Costo: </strong></span>{evento.price}</div>
+                                <div >
+                                    <img src={evento.poster} alt="poster-del-evento" className='poster'/>
+                                </div>
+
+                            </div>
+                        </div>
+                    </>
+                )
             })}
             </ul>
     </div>
--- src/ListaDeEventos/index.jsx
+++ src/ListaDeEventos/index.jsx
@@ -21,9 +21,10 @@ export const ListaDeEventos = () =>{
             placeName: 'Skatepark 360',
             address: 'Mina Bacís, Impregnadora Guadiana, 34217 Durango, Dgo.',
             phone: 'N/A',
-            price: 'N/A'
+            price: 'N/A',
+            poster: '/posters/poster-123.png'
         }, {
-            id: '2026_08_15_1',
+            id: '2026_08_15_2',
             start: '2026-08-15T17:00:00',
             end: '2026-08-15T19:00:00',
             title: 'Clases de Patinaje: Escuela roller 360',
@@ -32,9 +33,10 @@ export const ListaDeEventos = () =>{
             placeName: 'Skatepark 360',
             address: 'Mina Bacís, Impregnadora Guadiana, 34217 Durango, Dgo.',
             phone: 'N/A',
-            price: 'N/A'
+            price: 'N/A',
+            poster: '/posters/poster-123.png'
         },{
-            id: '2026_08_15_1',
+            id: '2026_08_15_3',
             start: '2026-08-15T17:00:00',
             end: '2026-08-15T19:00:00',
             title: 'Clases de Patinaje: Escuela roller 360',
@@ -43,9 +45,10 @@ export const ListaDeEventos = () =>{
             placeName: 'Skatepark 360',
             address: 'Mina Bacís, Impregnadora Guadiana, 34217 Durango, Dgo.',
             phone: 'N/A',
-            price: 'N/A'
+            price: 'N/A',
+            poster: '/posters/poster-123.png'
         },{
-            id: '2026_08_15_1',
+            id: '2026_08_15_4',
             start: '2026-08-15T17:00:00',
             end: '2026-08-15T19:00:00',
             title: 'Clases de Patinaje: Escuela roller 360',
@@ -54,9 +57,10 @@ export const ListaDeEventos = () =>{
             placeName: 'Skatepark 360',
             address: 'Mina Bacís, Impregnadora Guadiana, 34217 Durango, Dgo.',
             phone: 'N/A',
-            price: 'N/A'
+            price: 'N/A',
+            poster: '/posters/poster-123.png'
         }, {
-            id: '2026_08_15_1',
+            id: '2026_08_15_5',
             start: '2026-08-15T17:00:00',
             end: '2026-08-15T19:00:00',
             title: 'Clases de Patinaje: Escuela roller 360',
@@ -65,9 +69,10 @@ export const ListaDeEventos = () =>{
             placeName: 'Skatepark 360',
             address: 'Mina Bacís, Impregnadora Guadiana, 34217 Durango, Dgo.',
             phone: 'N/A',
-            price: 'N/A'
+            price: 'N/A',
+            poster: '/posters/poster-123.png'
         }, {
-            id: '2026_08_15_1',
+            id: '2026_08_15_6',
             start: '2026-08-15T17:00:00',
             end: '2026-08-15T19:00:00',
             title: 'Clases de Patinaje: Escuela roller 360',
@@ -76,9 +81,10 @@ export const ListaDeEventos = () =>{
             placeName: 'Skatepark 360',
             address: 'Mina Bacís, Impregnadora Guadiana, 34217 Durango, Dgo.',
             phone: 'N/A',
-            price: 'N/A'
+            price: 'N/A',
+            poster: '/posters/poster-123.png'
         },{
-            id: '2026_08_15_1',
+            id: '2026_08_15_7',
             start: '2026-08-15T17:00:00',
             end: '2026-08-15T19:00:00',
             title: 'Clases de Patinaje: Escuela roller 360',
@@ -87,7 +93,8 @@ export const ListaDeEventos = () =>{
             placeName: 'Skatepark 360',
             address: 'Mina Bacís, Impregnadora Guadiana, 34217 Durango, Dgo.',
             phone: 'N/A',
-            price: 'N/A'
+            price: 'N/A',
+            poster: '/posters/poster-123.png'
         },];
 
     return (
